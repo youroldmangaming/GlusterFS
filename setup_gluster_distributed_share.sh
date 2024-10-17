f404b8fb9bd8e8bb8abc9c2918ecf488c40c1c08
youroldmangaming <youroldmangaming@gmail.com>
2024-10-18 03:42:05 +1300

    Create setup_gluster_distributed_share.sh

---
+++ setup_gluster_distributed_share.sh
@@ -0,0 +1,10 @@
+cd/
+mkdir -p /glusterfs /mnt/glusterfs
+
+echo "$hostname:/glusterfs /mnt/glusterfs glusterfs defaults,_netdev,backupvolfile-server= 0 0" >> /etc/fstab
+
+
+chown -R root:docker /mnt
+systemctl daemon-reload
+mount -a
+mount.glusterfs $hostname:/glusterfs /mnt
